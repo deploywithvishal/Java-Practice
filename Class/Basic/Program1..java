907b8a1b80df2313db8a68f0859544db202c536b
VISHAL <kumarvishal14383@gmail.com>
2026-02-24 01:12:38 +0530

    1st program

--- Class/Basic/Program1..java
+++
@@ -1,7 +0,0 @@
-// Create a class Student with name and marks. Print details.
-
-class Program1{
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ Class/Basic/Program1.java
@@ -0,0 +1,14 @@
+// Create a class Student with name and marks. Print details.
+
+class Student {
+    String name = "Vishal Kumar";
+    int marks = 98;
+}
+
+class Program1 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println("Name: " + s.name);
+        System.out.println("Marks: " + s.marks);
+    }
+}
