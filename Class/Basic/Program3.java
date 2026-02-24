7e3a9f89fdda02727e9edaca8347b6b4e6e938ad
VISHAL <kumarvishal14383@gmail.com>
2026-02-25 00:38:36 +0530

    Class Basic Program3

--- Class/Basic/Program3.java
+++ Class/Basic/Program3.java
@@ -1,5 +1,21 @@
 // Create a class Circle with constructor.
+class Circle{
+    final double pi = 3.14;
+    double radius;
+
+    Circle(double r){
+        radius = r;
+    }
+
+    double radius(){
+        return pi*radius*radius;
+    }
+
+}
 
 public class Program3 {
-    
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+        System.out.println("Area of circle: "+ c.radius());
+    }
 }
