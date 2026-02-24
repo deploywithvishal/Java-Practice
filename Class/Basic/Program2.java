f2969f3e2483005b9b16ddd7b74c882a4a2ded83
VISHAL <kumarvishal14383@gmail.com>
2026-02-25 00:40:49 +0530

    Class Basic Program2 Update

--- Class/Basic/Program2.java
+++ Class/Basic/Program2.java
@@ -1,24 +1,20 @@
 // Create a class Rectangle with area method.
 
 class Rectangle {
-
     int length;
     int breadth;
 
-    Rectangle(int l, int b) {
-        length = l;
-        breadth = b;
-    }
-
-    int area() {
-        return length * breadth;
+    void area() {
+        int result = length * breadth;
+        System.out.println("Area of Rectangle: " + result);
     }
 }
 
-public class Program2 {
-
+class Program2 {
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle(5, 7);
-        System.out.println("Area: " + r1.area());
+        Rectangle r = new Rectangle();
+        r.length = 10;
+        r.breadth = 5;
+        r.area();
     }
 }
