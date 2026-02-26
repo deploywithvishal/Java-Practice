2ee692c4e368dff607b4f0e629104bf6b1e7ff36
VISHAL <kumarvishal14383@gmail.com>
2026-02-26 22:07:03 +0530

    Class Intermediate Problem1

--- Class/Intermediate/Program1.java
+++ Class/Intermediate/Program1.java
@@ -1,6 +1,28 @@
 // Create a class Employee with static company name.
+class Employee{
+    static String company = "DigiCore Future";
+    String name;
+    int age;
+
+    Employee(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void show(){
+        System.out.println("Company: "+company);
+        System.out.println("Name: "+name);
+        System.out.println("Age:"+age);
+        System.out.println("");
+    }
+}
 
 public class Program1 {
     public static void main(String[] args) {
+        Employee e1 = new Employee("Vishal", 22);
+        Employee e2 = new Employee("Shashi", 21);
+
+        e1.show();
+        e2.show();
     }
 }
