0b78c19a1f027e44cc56224589c5a90f7b1160dd
VISHAL <kumarvishal14383@gmail.com>
2026-02-26 22:10:37 +0530

    Class-Intermediate (Program2)

--- Class/Intermediate/Program2.java
+++ Class/Intermediate/Program2.java
@@ -1,4 +1,27 @@
 // Create a class Car with parameterized constructor.
-public class Program2 {
-    
+class Car{
+    String model;
+    int year;
+
+    Car(String model,int year){
+        this.model = model;
+        this.year = year;
+    }
+
+    void show(){
+        System.out.println("model: "+model);
+        System.out.println("year:"+year);
+        System.out.println("");
+    }
 }
+
+public class Program2 {
+    public static void main(String[] args) {
+        Car e1 = new Car("Datsun", 2025);
+        Car e2 = new Car("Toyota", 2026);
+
+        e1.show();
+        e2.show();
+    }
+}
+
